26d80fdeaf6c220b3f88dc9b1629f228b85476fa
menny1213 <mennykijzer15@gmail.com>
2016-12-28 16:33:52 +0200

    w

--- ChessProject/Rook.cpp
+++ ChessProject/Rook.cpp
@@ -4,36 +4,36 @@
 
 using namespace std;
 
+char Rook::repr() const
+{
+	return 'r';
+}
+
 Rook::Rook(Position pos, CastlingType _castlingAvaliablity) : Unit(pos)
 {
 	this->_castlingAvaliablity = _castlingAvaliablity;
+	_moved = false;
 }
 
 // All overrides inherited methods from Unit:
 vector<Position> Rook::pathToPosition(Position dest, bool enemyThere, Direction playerDirection) const
 {
+	return vector<Position>();
+}
 
+bool Rook::castlingAvaliable(CastlingType castlingType) const
+{
+	return longCastlingAvaliable() || shortCastlingAvaliable();
 }
 // Returns false only if long castling Rook and moved
 bool Rook::longCastlingAvaliable() const
 {
-	bool ans;
-	switch (this->_castlingAvaliablity)
-	{
-	case None:
-	case Short:
-		ans = false;
-		break;
-	case Long:
-		ans = true;
-		break;
-	}
-	return ans;
+	return (this->_castlingAvaliablity == Long) && !_moved;
 }
 // Returns false only if short castling Rook and moved
 bool Rook::shortCastlingAvaliable() const
 {
-
+	return (this->_castlingAvaliablity == Short) && !_moved;
 }
 // Does anything only if castlingType fits _castlingAvaliablity
 void Rook::commitCastling(CastlingType castlingType) const
--- ChessProject/Rook.h
+++ ChessProject/Rook.h
@@ -11,6 +11,7 @@ public:
 	// All overrides inherited methods from Unit:
 	virtual vector<Position> pathToPosition(Position dest, bool enemyThere, Direction playerDirection) const; // Throws exception if dest unreachable
 
+	virtual bool castlingAvaliable(CastlingType castlingType) const;
 	virtual bool longCastlingAvaliable() const; // Returns false only if long castling Rook and moved
 	virtual bool shortCastlingAvaliable() const; // Returns false only if short castling Rook and moved
 	virtual void commitCastling(CastlingType castlingType) const; // Does anything only if castlingType fits _castlingAvaliablity
