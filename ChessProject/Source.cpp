41264b5e961d1f699ebbdc40ea8d51bfc1354357
inbararan <inbar.aran@gmail.com>
2016-12-28 15:18:02 +0200

    new files

---
+++ ChessProject/Source.cpp
@@ -0,0 +1,68 @@
+#include "Pipe.h"
+#include <iostream>
+#include <thread>
+#include "Game.h"
+
+using namespace std;
+void main()
+{
+	srand(time_t(NULL));
+
+	system("start chessGraphics.exe");
+	Sleep(1000);
+	Pipe p;
+	bool isConnect = p.connect();
+	
+	string ans;
+	while (!isConnect)
+	{
+		cout << "cant connect to graphics" << endl;
+		cout << "Do you try to connect again or exit? (0-try again, 1-exit)" << endl;
+		cin >> ans;
+
+		if (ans == "0")
+		{
+			cout << "trying connect again.." << endl;
+			Sleep(5000);
+			isConnect = p.connect();
+		}
+		else 
+		{
+			p.close();
+			return;
+		}
+	}
+	
+	cout << "Connected to pipe." << endl;
+
+	char msgToGraphics[1024];
+	// msgToGraphics should contain the board string accord the protocol
+	Game game;
+
+	strcpy_s(msgToGraphics, game.getBoardRepr().c_str());
+	
+	p.sendMessageToGraphics(msgToGraphics);   // send the board string
+
+	// get message from graphics
+	string msgFromGraphics = p.getMessageFromGraphics();
+
+	while (msgFromGraphics != "quit")
+	{
+		// should handle the string the sent from graphics
+		// according the protocol. Ex: e2e4           (move e2 to e4)
+
+		cout << "Recieved from graphics: " << msgFromGraphics << endl;
+
+		strcpy_s(msgToGraphics, game.nextMove(msgFromGraphics).c_str()); // msgToGraphics should contain the result of the operation
+
+		cout << "Sending to graphics: " << msgToGraphics << endl;
+
+		// return result to graphics		
+		p.sendMessageToGraphics(msgToGraphics);   
+
+		// get message from graphics
+		msgFromGraphics = p.getMessageFromGraphics();
+	}
+
+	p.close();
+}
