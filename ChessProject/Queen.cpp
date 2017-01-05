fbf4345571e0d967c60347ba95e42b1be883e447
menny1213 <mennykijzer15@gmail.com>
2017-01-05 19:08:16 +0200

    nhewdsbj

---
+++ ChessProject/Queen.cpp
@@ -0,0 +1,40 @@
+
+#include "Queen.h"
+
+// Actually `static` - indpendent of instance properties
+char Queen::repr() const
+{
+	return 'q';
+}
+
+Queen::Queen(const Position& pos) : Unit(pos)
+{
+
+}
+
+	// All overrides inherited methods from Unit:
+vector<Position> Queen::pathToPosition(const Position& dest, MovementFlags& flags, bool enemyThere, Direction playerDirection) const
+{
+	vector<Position> positions;
+	CastlingType ct = None;
+	//is rook move
+	if ((dest.getFile() == _pos.getFile() || dest.getRank() == _pos.getRank()))
+	{
+		Rook* r = new Rook(_pos, ct);
+		positions = r->pathToPosition(dest,flags,enemyThere,playerDirection);
+		delete r;
+	}
+	//is bishop move
+	else if ((abs(_pos.getFile() - dest.getFile()) == abs(_pos.getRank() - dest.getRank())))
+	{
+		Bishop* b = new Bishop(_pos);
+		positions = b->pathToPosition(dest, flags, enemyThere, playerDirection);
+		delete b;
+	}
+	else
+	{
+		throw UnreachablePositionException();
+	}
+	return positions;
+}
+// Throws exception if dest unreachable
